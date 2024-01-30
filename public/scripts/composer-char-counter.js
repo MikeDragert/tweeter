4ab167715658b3287a1fcc3926bf5d4cb73312d9
Mike Dragert <mikedragert@gmail.com>
2024-01-30 17:39:32 -0500

    Add features from day two activies

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+  
+  $("#tweet-text").on("input", function() {
+    let newLength = $(this).val().length;
+    if (newLength > 140) {
+      $(this).parent().find(".counter").css({"color": "red"});
+      // this was to lock it at 140, which works but was not the requirement
+      // $(this).val($(this).val().substring(0,140))
+      // newLength = 140;
+    } else {
+      $(this).parent().find(".counter").css({"color": ""});
+    }
+    $(this).parent().find(".counter").val(140-newLength);
+  });
+})
